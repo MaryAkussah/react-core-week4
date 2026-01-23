2ba5796726aaee41245dfeddbcbe44a20f153bdc
MaryAkussah <maryakussah123@gmail.com>
2026-01-23 17:02:12 +0000

    React Router for multiple pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,6 @@
+function About() {
+    return (
+        <h2>Heyya This is the F****** About Page</h2>
+    )
+}
+export default About;
---
+++ src/pages/Posts.tsx
@@ -0,0 +1,6 @@
+function Posts() {
+    return (
+        <h2>Duhhh the Post Page</h2>
+    )
+}
+export default Posts;
